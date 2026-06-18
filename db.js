5f949f10a88ba102b97a64dfbd83edd0741695e7
Rahul Jawale <jawalerahul080@gmail.com>
2026-06-18 17:29:36 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ var mysql=require('mysql2');
 var util=require('util');
 
 var conn=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'ROOT',
-    database:'dental'
+    host:'b72hbsxzirvqq8peyfno-mysql.services.clever-cloud.com',
+    user:'ujin5afs4gipyxtv',
+    password:'ujin5afs4gipyxtv',
+    database:'b72hbsxzirvqq8peyfno'
 });
 
 var exe=util.promisify(conn.query).bind(conn);
